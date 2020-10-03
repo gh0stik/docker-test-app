4a1d6c4031e7a3f22c4ef6a3583c86bcca7004de
root <nicolai.putintsev@gmail.com>
2020-10-04 01:30:00 +0300

    modified travis yml and exposed port

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ class TestTest_func(unittest.TestCase):
 
 	def test_test_func(self):
 		results = test_func(2, 7)
-		self.assertEqual(results, 10)
+		self.assertEqual(results, 9)
 
 
 	def test_test_func2(self):
